a790310a229a64f975a3fdfedf71b8cb72054b2b
Katorgin Maxim <KatorginMI@yandex.ru>
2026-07-08 08:44:29 +0300

    2.3.5.2. Фильтры [#505607]

---
+++ scripts/505607_filters/1_insert_new_orders.sql
@@ -0,0 +1,3 @@
+INSERT INTO orders (user_id, status)
+VALUES (1, 'CANCELLED'),
+       (1, 'CANCELLED');
---
+++ scripts/505607_filters/2_select_queries.sql
@@ -0,0 +1,36 @@
+SELECT id, name, price, is_active
+FROM products
+WHERE is_active = true;
+
+SELECT id, name, price
+FROM products
+WHERE price BETWEEN 10_000 AND 100_000;
+
+SELECT id, user_id, status, created_at
+FROM orders
+WHERE status IN ('NEW', 'PAID');
+
+SELECT id, user_id, status, created_at
+FROM orders
+WHERE user_id = 1
+  AND status <> 'CANCELLED';
+
+SELECT id, name, email, created_at
+FROM users
+WHERE created_at BETWEEN
+          TIMESTAMPTZ '2026-01-01 00:00:00+00' AND
+          TIMESTAMPTZ '2026-12-31 23:59:59+00';
+
+SELECT id, name, price
+FROM products
+WHERE price < 20_000
+   OR price > 80_000;
+
+SELECT id, name, price
+FROM products
+WHERE is_active = true
+  AND (price < 3_000 OR price > 150_000)
+
+
+
+
